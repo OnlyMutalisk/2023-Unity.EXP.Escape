2620393f4be38167aa63dbd8118a87f2cb9fd217
Onlymutalisk <onlymutalisk@gmail.com>
2023-05-09 21:41:58 +0900

    Style: Form a base working environment

---
+++ Assets/Class/CutScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// 클래스 주석
+/// 요구 패키지 Example.TEST
+/// </summary>
+public class CutScene : MonoBehaviour
+{
+    // 메소드 주석
+    public void TestMethod()
+    {
+
+    }
+
+    #region 내부구현 (private)
+
+    #endregion
+}
