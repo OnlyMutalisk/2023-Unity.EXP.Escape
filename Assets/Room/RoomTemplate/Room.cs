54a686d976e39dab960fe83e2ce7c2eaa8e08bde
Onlymutalisk <onlymutalisk@gmail.com>
2023-05-16 02:37:51 +0900

    Feat: Make RoadViews & Fade In / Out

---
+++ Assets/Room/RoomTemplate/Room.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Room : MonoBehaviour
+{
+    public string roomName;
+    // public RoomObject roomObject; 서브 프로그래머 구현 예정
+    // public CutScene roomCutScene; 서브 프로그래머 구현 예정
+    public AudioSource moveEffectSound;
+    // 퀵슬롯
+
+    // 방 입장 효과음 재생()
+    // 상호작용 대사 출력() : 아이템 획득시 이미지도 출력
+    // 독백 대사 출력() : 캐릭터 컷신 이미지 팝업
+    // 퀵슬롯() : 로드뷰 & 인벤토리 & 메뉴 & 로드뷰 ON / OFF
+    // 방 나감 fade out()
+
+    private void Start()
+    {
+        // moveEffectSound 재생
+
+        // 상호작용 
+    }
+
+    #region 내부 구현 (private)
+
+    #endregion
+}
