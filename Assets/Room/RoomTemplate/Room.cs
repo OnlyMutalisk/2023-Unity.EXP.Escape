0f7b9b8d881fe1348c3b968146707239982b81e8
Onlymutalisk <onlymutalisk@gmail.com>
2023-05-18 02:01:11 +0900

    Fix: Refactory the Fade In / Out

--- Assets/Room/RoomTemplate/Room.cs
+++ Assets/Room/RoomTemplate/Room.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -23,9 +24,26 @@ public class Room : MonoBehaviour
         // moveEffectSound 재생
 
         // 상호작용 
+
+        // 씬을 페이드 인 합니다.
+        FadeIn();
     }
 
     #region 내부 구현 (private)
+
+    private void FadeIn()
+    {
+        // 검은 화면 오브젝트인 "FadeIn" 를 페이드 아웃하여, 씬을 페이드 인 시킴
+        // FadeIn 오브젝트 선택
+        GameObject gameObject = GameObject.Find("FadeIn");
+
+        // 이미지 컴포넌트의 Color 값을 건드려, 검은 화면으로 만듦.
+        gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+
+        // 이제 FadeIn 오브젝트를 페이드 아웃
+        이미지_이펙트 imageEffect = new 이미지_이펙트();
+        StartCoroutine(imageEffect.페이드_아웃("FadeIn", 2, 60));
+    }
 
     #endregion
 }
