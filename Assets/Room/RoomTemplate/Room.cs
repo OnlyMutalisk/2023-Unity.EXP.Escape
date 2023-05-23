33f71e24c9e272742b609ea5fbc03b63d85fc960
Onlymutalisk <onlymutalisk@gmail.com>
2023-05-24 01:46:27 +0900

    Feat: Add Print & Monologue

--- Assets/Room/RoomTemplate/Room.cs
+++ Assets/Room/RoomTemplate/Room.cs
@@ -1,38 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor.EditorTools;
 using UnityEngine;
+using System;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor.VersionControl;
 
 public class Room : MonoBehaviour
 {
-    public string roomName;
-    // public RoomObject roomObject; 서브 프로그래머 구현 예정
-    // public CutScene roomCutScene; 서브 프로그래머 구현 예정
-    public AudioSource moveEffectSound;
-    // 퀵슬롯
-
-    // 방 입장 효과음 재생()
-    // 상호작용 대사 출력() : 아이템 획득시 이미지도 출력
-    // 독백 대사 출력() : 캐릭터 컷신 이미지 팝업
-    // 퀵슬롯() : 로드뷰 & 인벤토리 & 메뉴 & 로드뷰 ON / OFF
-    // 방 나감 fade out()
-
-    private void Start()
-    {
-        // moveEffectSound 재생
-
-        // 상호작용 
-
-        // 씬을 페이드 인 합니다.
-        StartCoroutine(FadeIn());
-
-        // 씬을 페이드 아웃 합니다.
-        // fadeInOut.FadeOut();
-
-    }
-
     #region 내부 구현 (private)
 
     private IEnumerator FadeIn()
@@ -47,15 +24,28 @@ public class Room : MonoBehaviour
         // 이미지 컴포넌트의 Color 값을 건드려, 검은 화면으로 만듦.
         gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 1);
 
-        // 이제 FadeInOut 오브젝트를 time 초 동안 페이드 아웃
-        float time = 1;
+        // 이제 FadeInOut 오브젝트를 RoomSettings 에 정의된 시간 동안 페이드 아웃
         이미지_이펙트 imageEffect = new 이미지_이펙트();
-        StartCoroutine(imageEffect.페이드_아웃("FadeInOut", time, 60));
+        StartCoroutine(imageEffect.페이드_아웃("FadeInOut", RoomSettings.floatSceneFadeInOutTime, 60));
 
         // time 초 동안 대기 후, FadeInOut 오브젝트를 비활성화 시켜 다른 오브젝트를 가리지 않게 함.
-        yield return new WaitForSeconds(time * (float)2);
+        yield return new WaitForSeconds(RoomSettings.floatSceneFadeInOutTime * (float)1.5);
         gameObject.SetActive(false);
     }
 
     #endregion
+
+    public AudioSource moveEffectSound;
+    // 퀵슬롯
+
+    // 독백 대사 출력() : 캐릭터 컷신 이미지 팝업
+    // 퀵슬롯() : 로드뷰 & 인벤토리 & 메뉴 & 로드뷰 ON / OFF
+
+    private void Start()
+    {
+        // moveEffectSound 재생 (자동, 코드 원래 없음)
+
+        // 씬을 페이드 인 합니다.
+        StartCoroutine(FadeIn());
+    }
 }
---
+++ Assets/Room/RoomTemplate/Scripts/Monologue.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monologue : MonoBehaviour
+{
+    #region 내부 구현
+
+    // 코루틴 동기화 플래그 입니다.
+    public bool boolCoroutineSynchronizeFlag = true;
+
+    public int intSkipFlag = 0;
+
+    // 인스펙터에서 연결합니다.
+    public Text componentText;
+    public GameObject gameObjectPanelButton;
+    public GameObject gameObjectArrow;
+
+    // 독백의 이터레이터 입니다.
+    public int intCount;
+
+    // 각 글자 시간 간격을 설정 합니다.
+    private float floatTypingSpeed = 0.1f;
+
+    // 독백 덩어리를 출력합니다.
+    private IEnumerator IEnumeratorPrintMonologue(LinkedList<string> 독백)
+    {
+        // 코루틴 접근을 차단합니다.
+        boolCoroutineSynchronizeFlag = false;
+
+        // 메시지 패널을 활성화 합니다.
+        gameObjectPanelButton.SetActive(true);
+
+        // 아직 출력돼야 할 메시지가 남았다면
+        if (intCount < 독백.Count)
+        {
+            // 텍스트를 초기화 합니다.
+            componentText.text = "";
+
+            // Arrow 를 비활성화 합니다.
+            gameObjectArrow.SetActive(false);
+
+            // 글자 수를 0 부터 점점 늘려갑니다.
+            int characterIndex = 0;
+
+            // 모든 글자가 출력될 때 까지 반복합니다.
+            while (characterIndex < 독백.First.Value.Length)
+            {
+                // 첫 터치 시 출력은
+                if (intSkipFlag == 1)
+                {
+                    // 글자 갯수를 하나 더 늘립니다.
+                    componentText.text += 독백.First.Value[characterIndex];
+                    characterIndex++;
+
+                    // 글자 간 간격 시간 만큼 대기합니다.
+                    yield return new WaitForSeconds(floatTypingSpeed);
+                }
+                // 두번째 터치 시 부터는
+                else if (intSkipFlag >= 2)
+                {
+                    // 모든 글자를 한번에 출력합니다.
+                    componentText.text = 독백.First.Value;
+                    break;
+                }
+            }
+
+            // 사용한 대사를 지우고, 리스트의 마지막에 다시 추가해줍니다.
+            독백.AddLast(독백.First.Value);
+            독백.RemoveFirst();
+
+            // Arrow 를 활성화 합니다.
+            gameObjectArrow.SetActive(true);
+
+            // 스킵 플래그를 초기화 합니다.
+            intSkipFlag = 0;
+
+            // 이터레이터를 다음으로 옮깁니다.
+            intCount++;
+        }
+        // 모두 다 출력을 완료 했다면
+        else
+        {
+            // 독백 패널을 닫습니다.
+            gameObjectPanelButton.SetActive(false);
+
+            // 스킵 플래그를 초기화 합니다.
+            intSkipFlag = 0;
+
+            // 이터레이터를 초기화 합니다.
+            intCount = 0;
+        }
+
+        // 코루틴 접근을 허용합니다.
+        boolCoroutineSynchronizeFlag = true;
+    }
+
+    #endregion
+
+    /// <summary>
+    /// <br>지정된 독백 메시지를 순차 출력합니다.</br>
+    /// </summary>
+    public void PrintMonologue()
+    {
+        intSkipFlag++;
+
+        if (boolCoroutineSynchronizeFlag == true)
+        {
+            StartCoroutine(IEnumeratorPrintMonologue(Messages.map0_독백));
+        }
+    }
+}
