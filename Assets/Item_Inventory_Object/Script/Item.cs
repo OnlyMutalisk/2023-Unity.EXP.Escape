9463322c0db0889df214027be9e04bfc28b5ade5
hyesuhan <heasoo88@gmail.com>
2023-05-23 16:08:48 +0900

    Inventory Button & Item Slots

--- Assets/Item_Inventory_Object/Script/Item.cs
+++ Assets/Item_Inventory_Object/Script/Item.cs
@@ -2,23 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-//아이템에 필요한 것
-//획득 시 아이템 이름, 이미지, 간단한 설명 필요
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
 public class Item : ScriptableObject
 {
-    //아이템의 정보를 담을 스크립트
-    public string itemName; 
-    public Sprite itemImage;
-    public string itemText;
-    public ItemType itemtype;
-
-    public enum ItemType //소모품인지 아니면 계속 가지고 있을지 설정
-    { 
-        Use, Equip, Etc
-    }
-
-
+    public int id;
+    public string itemName;
+    public Sprite icon;
 }
